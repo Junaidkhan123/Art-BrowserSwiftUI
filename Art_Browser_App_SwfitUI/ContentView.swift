6af6792a613396d0ca2e8d09048703eb4df2f74c
Muhammad  Junaid <junaid123.jk78@gmail.com>
2020-11-29 23:16:26 +0500

    Initial Commit

---
+++ Art_Browser_App_SwfitUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Art_Browser_App_SwfitUI
+//
+//  Created by Jhon Khan on 29/11/2020.
+//  Copyright © 2020 Jhon Khan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
